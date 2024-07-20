90d20ace615f0cbd5954cd1266bddf0d45137ac4
ehsan2793 <ehsan2793@gmail.com>
2024-07-19 19:25:15 -0700

    Initial Commit

---
+++ timer/timerApp.swift
@@ -0,0 +1,17 @@
+//
+//  timerApp.swift
+//  timer
+//
+//  Created by Ehsan Rahimi on 7/19/24.
+//
+
+import SwiftUI
+
+@main
+struct timerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
